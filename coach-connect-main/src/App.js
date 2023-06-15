f2d6896532e17443481b25be18799fca8b687390
Jeremiah Williams <williamsjeremiahkenneth@gmail.com>
2023-06-15 17:07:13 -0400

    Implemented basic Supabase setup

--- coach-connect-main/src/App.js
+++ coach-connect-main/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navbar from './Components/NavBar/navbar';
 import { ScrollViewClients } from './Components/ClientSearch/scrollViewClients';
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("https://mpvvmognqqbdrkaxwxpr.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wdnZtb2ducXFiZHJrYXh3eHByIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODY4NTcxNDYsImV4cCI6MjAwMjQzMzE0Nn0.LdwvrpeNPNTolUxpY26nDSEn-qNHMn6d-KGcCHHocKc");
 
 
 const LeftSideComponent = () => {
